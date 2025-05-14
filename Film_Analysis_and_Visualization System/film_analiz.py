fc9febeee7b59c2fb9377f269f77f4ba7771a4b8
Mesut Şahin <mesutsahin0100@gmail.com>
2025-05-14 14:17:14 +0300

    Update web interface added

--- Film_Analysis_and_Visualization System/film_analiz.py
+++ Film_Analysis_and_Visualization System/film_analiz.py
@@ -1,39 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import title
+import streamlit as st
+from collections import Counter
 
 
 class FilmAnaliz:
     def __init__(self):
         self.df = pd.read_csv("netflix_titles.csv")
 
-    def tur_filtrele(self,tur):
-        filtreli = self.df[self.df["listed_in"].str.contains(tur,na=False)]
-        print(filtreli[["title","type","release_year"]].head(10))
+    def tur_filtrele(self, tur):
+        filtreli = self.df[self.df["listed_in"].str.contains(tur, na=False)]
+        st.dataframe(filtreli[["title", "type", "release_year"]].head(10))
 
-    def yil_araligi(self,baslangic,bitis):
+    def yil_araligi(self, baslangic, bitis):
         secim = self.df[(self.df["release_year"] >= baslangic) & (self.df["release_year"] <= bitis)]
-        print(secim[["release_year","title","country"]].head(10))
-    def ulke_filtrele(self,ulke_adi):
-        secim = self.df[self.df["country"].str.contains(ulke_adi,na=False)]
-        print(secim[["title","country"]].head(20))
+        st.dataframe(secim[["release_year", "title", "country"]].head(10))
+
+    def ulke_filtrele(self, ulke_adi):
+        secim = self.df[self.df["country"].str.contains(ulke_adi, na=False)]
+        st.dataframe(secim[["title", "country"]].head(20))
 
     def tur_grafigi(self):
         sayim = self.df["type"].value_counts()
-        sayim.plot.pie(autopct = "%1.1f%%")
-        plt.title("Film vs Dizi oranı")
-        plt.show()
+        sayim.plot.pie(autopct="%1.1f%%")
+        plt.title("Film vs Dizi Oranı")
+        st.pyplot(plt)
+        plt.clf()
+
     def yillara_gore_bar(self):
         grafik = self.df["release_year"].value_counts().sort_index()
-        grafik.plot(kind="bar",figsize=(10,5))
+        grafik.plot(kind="bar", figsize=(10, 5))
         plt.xlabel("Yıl")
         plt.ylabel("İçerik Sayısı")
         plt.title("Yıllara Göre İçerik Sayısı")
         plt.tight_layout()
-        plt.show()
+        st.pyplot(plt)
+        plt.clf()
 
     def populer_turler(self):
-        from collections import Counter
         tur_listesi = self.df['listed_in'].dropna().str.split(', ')
         tum_turler = [tur for liste in tur_listesi for tur in liste]
         sayim = Counter(tum_turler)
@@ -42,7 +46,8 @@ class FilmAnaliz:
         plt.bar(en_populer.keys(), en_populer.values())
         plt.xticks(rotation=45)
         plt.title("En Popüler Türler")
-        plt.show()
+        st.pyplot(plt)
+        plt.clf()
 
     def yillik_trend(self):
         movie = self.df[self.df['type'] == "Movie"]['release_year'].value_counts().sort_index()
@@ -55,13 +60,14 @@ class FilmAnaliz:
         plt.legend()
         plt.title("Yıllara Göre Film & Dizi Trendleri")
         plt.grid(True)
-        plt.show()
+        st.pyplot(plt)
+        plt.clf()
 
     def baslik_arama(self, kelime):
         secim = self.df[self.df['title'].str.contains(kelime, case=False, na=False)]
-        print(secim[['title', 'type', 'release_year']].head())
+        st.dataframe(secim[['title', 'type', 'release_year']].head())
 
-    def yeni_ekle(self,title,year,country,types,listed_in):
+    def yeni_ekle(self, title, year, country, types, listed_in):
         yeni = {
             'title': title,
             'release_year': year,
@@ -69,11 +75,10 @@ class FilmAnaliz:
             'type': types,
             'listed_in': listed_in
         }
-        self.df = self.df.append(yeni,ignore_index=True)
-        print("Ekleme İşlemi Başarılı....")
-        self.kaydet(yeni)
+        self.df = pd.concat([self.df, pd.DataFrame([yeni])], ignore_index=True)
+        st.success("Ekleme İşlemi Başarılı!")
+        self.kaydet()
 
-    def kaydet(self,yeni_dosya):
-        self.df.to_csv(yeni_dosya,index=False)
-        print("Kaydedildi....")
-
+    def kaydet(self):
+        self.df.to_csv("netflix_titles.csv", index=False)
+        st.info("Veriler Kaydedildi.")
--- Film_Analysis_and_Visualization System/main.py
+++ Film_Analysis_and_Visualization System/main.py
@@ -1,43 +1,58 @@
+import streamlit as st
 from film_analiz import FilmAnaliz
 
-film_sinifi = FilmAnaliz()
+st.set_page_config(page_title="Netflix Film Analiz", layout="centered")
 
-while True:
-    print("""
-    --- Netflix Analiz Sistemine Hoş Geldiniz ---
+analiz = FilmAnaliz()
 
-    1. Belirli türdeki filmleri listele
-    2. Belirli yıl aralığında çıkanları listele
-    3. Ülkeye göre filtrele
-    4. İçerik türü dağılımı grafiğini çiz
-    5. Yıllara göre içerik artışı grafiği
-    6. Popüler türleri listele
-    7. Yıllık trend grafiği
-    8. Başlık Arama
-    9. Veri Ekle
-    6. Programı kapat
+st.title("🎬 Netflix Film & Dizi Analizi")
 
-    """)
-    secim = int(input())
-    if secim==1:
-        film_sinifi.tur_filtrele(input("Tür: "))
-    elif secim==2:
-        film_sinifi.yil_araligi(int(input("Başlangıç: ")),int(input("Bitiş: ")))
-    elif secim==3:
-        film_sinifi.ulke_filtrele(input("Ülke Adı: "))
-    elif secim==4:
-        film_sinifi.tur_grafigi()
-    elif secim==5:
-        film_sinifi.yillara_gore_bar()
-    elif secim==6:
-        film_sinifi.populer_turler()
-    elif secim==7:
-        film_sinifi.yillik_trend()
-    elif secim==8:
-        film_sinifi.baslik_arama(input("Başlık: "))
-    elif secim==9:
-        film_sinifi.yeni_ekle(input("Başlık: "),int(input("Yıl: ")),input("Ülke: "),input("Tür: "),input("Liste türü: "))
-    elif secim==10:
-        break
-    else:
-        print("Lütfen geçerli seçim yapınız....")
+menu = st.sidebar.selectbox("İşlem Seçiniz", [
+    "Tür Filtrele", "Yıl Aralığına Göre", "Ülkeye Göre Filtrele",
+    "Yıllara Göre Bar Grafik", "Film/Dizi Oranı", "Popüler Türler",
+    "Yıllık Trend (Çizgi Grafik)", "Başlığa Göre Arama", "Yeni İçerik Ekle"
+])
+
+if menu == "Tür Filtrele":
+    tur = st.text_input("Tür giriniz (örnek: Drama, Comedy):")
+    if st.button("Filtrele"):
+        st.write("Filtrelenen Sonuçlar:")
+        analiz.tur_filtrele(tur)
+
+elif menu == "Yıl Aralığına Göre":
+    baslangic = st.number_input("Başlangıç Yılı", 1950, 2025, 2000)
+    bitis = st.number_input("Bitiş Yılı", 1950, 2025, 2020)
+    if st.button("Göster"):
+        analiz.yil_araligi(baslangic, bitis)
+
+elif menu == "Ülkeye Göre Filtrele":
+    ulke = st.text_input("Ülke giriniz (örnek: Turkey, India):")
+    if st.button("Filtrele"):
+        analiz.ulke_filtrele(ulke)
+
+elif menu == "Yıllara Göre Bar Grafik":
+    st.write("Yıllara göre içerik sayısı:")
+    analiz.yillara_gore_bar()
+
+elif menu == "Film/Dizi Oranı":
+    analiz.tur_grafigi()
+
+elif menu == "Popüler Türler":
+    analiz.populer_turler()
+
+elif menu == "Yıllık Trend (Çizgi Grafik)":
+    analiz.yillik_trend()
+
+elif menu == "Başlığa Göre Arama":
+    kelime = st.text_input("Başlıkta geçen kelime:")
+    if st.button("Ara"):
+        analiz.baslik_arama(kelime)
+
+elif menu == "Yeni İçerik Ekle":
+    title = st.text_input("İçerik Başlığı")
+    year = st.number_input("Yayın Yılı", min_value=1900, max_value=2025, value=2020)
+    country = st.text_input("Ülke")
+    types = st.selectbox("Tür", ["Movie", "TV Show"])
+    listed_in = st.text_input("Kategori (örnek: Drama, Action)")
+    if st.button("Ekle"):
+        analiz.yeni_ekle(title, year, country, types, listed_in)
